d484e3786f79410a1b05fe571db562fc4f39501a
WoelliA <andreas.woelfl@stud.uni-regensburg.de>
2016-12-12 16:59:30 +0900

    Add project files.

---
+++ Band2Sensor/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Band.Portable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Band2Sensor
+{
+    
+    [Activity(Label = "Band2Sensor", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        async Task ConnectToBandAsync()
+        { 
+        BandClientManager bandClientManager = BandClientManager.Instance;
+        // query the service for paired devices
+       
+        var pairedBands = await bandClientManager.GetPairedBandsAsync();
+        // connect to the first device
+        var bandInfo = pairedBands.FirstOrDefault();
+        var bandClient = await bandClientManager.ConnectAsync(bandInfo);
+           
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
